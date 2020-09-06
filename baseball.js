0801e9bdba8af10be19ef671cf394f4f3e3cc6d7
parkcheayeon <70843067+parkcheayeon@users.noreply.github.com>
2020-09-06 21:14:40 +0900

    Add files via upload

---
+++ baseball.js
@@ -0,0 +1,78 @@
+
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    inputNum = document.querySelector(".js-inputNum"),
+    randomNum = document.querySelector(".js-randomNum"),
+    point = document.querySelector(".js-point");
+
+
+let count = 0;
+
+
+function compareNum(input, random) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < 3; i++) {
+        if (random.includes(input[i])) {
+            ball += 1;
+        }
+        if (input[i] === random[i]) {
+            strike += 1;
+        }
+    }
+    if (strike === 3) {
+        alert('축하합니다!🎉')
+    }
+    if (ball === 0) {
+        alert('숫자가 하나도 안맞네요 🤢')
+    }
+    point.innerText = `${strike} Strike ${ball} Ball`;
+}
+
+
+function genRandom() {
+    let number = "";
+    for (let i = 0; i < 3; i++) {
+        const num = Math.floor(Math.random() * 10);
+        number += num;
+    }
+    return number;
+}
+
+
+function paintNumber(text) {
+    inputNum.innerText = `Your number is ${text}`;
+}
+
+
+function isNumber(text) {
+    let regExp = /^[0-9]{3}$/;
+    return regExp.test(text);
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    input.value = "";
+    if (!isNumber(currentValue)) {
+        alert("세자리숫자를 입력해주세요!")
+    } else {
+        paintNumber(currentValue);
+        const randomValue = genRandom();
+        randomNum.innerText = `My number is ${randomValue}`;
+        compareNum(currentValue, randomValue);
+        count += 1;
+        if (count === 10) {
+            alert('실패입니다😅');
+            window.location.reload();
+        }
+    }
+}
+
+
+function init() {
+    alert('3 Strike 도전! 숫자를 입력해주세요!')
+    form.addEventListener("submit", handleSubmit);
+}
+init();
